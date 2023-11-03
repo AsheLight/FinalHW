035c4d5a369f2aaccbd2600e0640a408bab5cba0
AsheLight <nagornov-1995@inbox.ru>
2023-11-03 11:08:13 +0300

    Stage 2 has finished

--- FinalHW.cs
+++ FinalHW.cs
@@ -19,3 +19,21 @@ switch (fromUser)
 
 }
 
+int lenNewArray = 0;
+for (int i =0; i <= array.length -1; i++)
+{
+    if (array[i].length <=3) lenNewArray++;
+}
+
+string [] newArray = new string [lenNewArray];
+int idx=0;
+
+for (int i = 0; i <= array.length - 1; i++)
+{
+    if (array[i].length <=3)
+    {
+        newArray[idx] = array[i];
+        idx++;
+    }
+}
+
